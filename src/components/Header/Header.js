749a99f9ea939124d13ebb45f5b8b379c62523ad
iamdevlinph <iamdevlinph@gmail.com>
2017-04-08 22:21:43 +0800

    Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,15 +3,24 @@ import { IndexLink, Link } from 'react-router'
 import './Header.scss'
 
 export const Header = () => (
-  <div>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='route--active'>
-      About
-    </IndexLink>
-    {' · '}
-    <Link to='/calculator' activeClassName='route--active'>
-      Calculator
-    </Link>
+  <div className='header'>
+    <nav className='nav'>
+      <div className='nav-wrapper'>
+        <a href='javascript:;' className='brand-logo right waves-effect'>React Calculator</a>
+        <ul id='nav-mobile' className='left hide-on-med-and-down'>
+          <li>
+            <IndexLink to='/' activeClassName='nav__active' className='waves-effect'>
+              About
+            </IndexLink>
+          </li>
+          <li>
+            <Link to='/calculator' activeClassName='nav__active' className='waves-effect'>
+              Calculator
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 )
 
