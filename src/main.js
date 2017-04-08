a499a27f4121274c647a9fbd81d8d69345b0b067
iamdevlinph <iamdevlinph@gmail.com>
2017-04-08 22:07:18 +0800

    Init materialize

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import createStore from './store/createStore'
 import AppContainer from './containers/AppContainer'
 
+import 'materialize-css/bin/materialize.js'
+
 // ========================================================
 // Store Instantiation
 // ========================================================
