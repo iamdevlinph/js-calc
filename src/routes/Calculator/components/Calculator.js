992ff614b4bf19fa94df074db28cafe79522d70a
iamdevlinph <iamdevlinph@gmail.com>
2017-04-09 17:57:20 +0800

    Create my own calculator

--- src/routes/Calculator/components/Calculator.js
+++ src/routes/Calculator/components/Calculator.js
@@ -4,48 +4,156 @@ import './Calculator.scss'
 
 export const Calculator = (props) => (
   <div className='calculator'>
-    <div id='background'>
-      <div id='result' />
-
-      <div id='main'>
-        <div id='first-rows'>
-          <button className='del-bg' id='delete'>Del</button>
-          <button value='%' className='btn-style operator opera-bg fall-back'>%</button>
-          <button value='+' className='btn-style opera-bg value align operator'>+</button>
+    <div className='row'>
+      <div className='col s6'>
+        <div className='calculator__container'>
+          <table className='calculator__table'>
+            <tbody>
+              <tr>
+                <td colSpan='4'>
+                  <span className='calculator__result'>CALCULATOR RESULT</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className='btn single'>DEL</button>
+                </td>
+                <td>
+                  <button className='btn single'>/</button>
+                </td>
+                <td>
+                  <button className='btn single'>*</button>
+                </td>
+                <td>
+                  <button className='btn single'>-</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className='btn single'>7</button>
+                </td>
+                <td>
+                  <button className='btn single'>8</button>
+                </td>
+                <td>
+                  <button className='btn single'>9</button>
+                </td>
+                <td rowSpan='2'>
+                  <button className='btn double__row'>+</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className='btn single'>4</button>
+                </td>
+                <td>
+                  <button className='btn single'>5</button>
+                </td>
+                <td>
+                  <button className='btn single'>6</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className='btn single'>1</button>
+                </td>
+                <td>
+                  <button className='btn single'>2</button>
+                </td>
+                <td>
+                  <button className='btn single'>3</button>
+                </td>
+                <td rowSpan='2'>
+                  <button className='btn double__row'>=</button>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan='2'>
+                  <button className='btn double__col'>0</button>
+                </td>
+                <td>
+                  <button className='btn single'>.</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-
-        <div className='rows'>
-          <button value='7' className='btn-style num-bg num first-child'>7</button>
-          <button value='8' className='btn-style num-bg num'>8</button>
-          <button value='9' className='btn-style num-bg num'>9</button>
-          <button value='-' className='btn-style opera-bg operator'>-</button>
+      </div>
+      <div className='col s6'>
+        <div className='calculator__container'>
+          <table className='calculator__table'>
+            <tbody>
+              <tr>
+                <td colSpan='4'>
+                  <span className='calculator__result'>CALCULATOR RESULT</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className='btn single'>DEL</button>
+                </td>
+                <td>
+                  <button className='btn single'>/</button>
+                </td>
+                <td>
+                  <button className='btn single'>*</button>
+                </td>
+                <td>
+                  <button className='btn single'>-</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className='btn single'>7</button>
+                </td>
+                <td>
+                  <button className='btn single'>8</button>
+                </td>
+                <td>
+                  <button className='btn single'>9</button>
+                </td>
+                <td rowSpan='2'>
+                  <button className='btn double__row'>+</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className='btn single'>4</button>
+                </td>
+                <td>
+                  <button className='btn single'>5</button>
+                </td>
+                <td>
+                  <button className='btn single'>6</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <button className='btn single'>1</button>
+                </td>
+                <td>
+                  <button className='btn single'>2</button>
+                </td>
+                <td>
+                  <button className='btn single'>3</button>
+                </td>
+                <td rowSpan='2'>
+                  <button className='btn double__row'>=</button>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan='2'>
+                  <button className='btn double__col'>0</button>
+                </td>
+                <td>
+                  <button className='btn single'>.</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-
-        <div className='rows'>
-          <button value='4' className='btn-style num-bg num first-child'>4</button>
-          <button value='5' className='btn-style num-bg num'>5</button>
-          <button value='6' className='btn-style num-bg num'>6</button>
-          <button value='*' className='btn-style opera-bg operator'>x</button>
-        </div>
-
-        <div className='rows'>
-          <button value='1' className='btn-style num-bg num first-child'>1</button>
-          <button value='2' className='btn-style num-bg num'>2</button>
-          <button value='3' className='btn-style num-bg num'>3</button>
-          <button value='/' className='btn-style opera-bg operator'>/</button>
-        </div>
-
-        <div className='rows'>
-          <button value='0' className='num-bg zero' id='delete'>0</button>
-          <button value='.' className='btn-style num-bg period fall-back'>.</button>
-          <button value='=' id='eqn-bg' className='eqn align'>=</button>
-        </div>
-
       </div>
     </div>
-    <span className='calculator__disclaimer'>
-      (c) https://codepen.io/ckm100/pen/dPLyjZ
-    </span>
   </div>
 )
 
